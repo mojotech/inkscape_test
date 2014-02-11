1190c0dcafb3d883b8b3be470d255f549e5c2758
dufresne@mojotech.com <dufresne@mojotech.com>
2014-02-11 15:13:31 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InkscapeTest::Application.config.secret_token = '452344ca1bc713af522c0ce2f96143e7f5cd5d07ae52f1ad6d5ae4bdc3a7f26638222185cdaffcac0161377dc6cdb681ec6ab920b35d8a105125deac6d21895b'
